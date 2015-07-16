8e8279419e18e81f8ff5fde17147d35a82e000b2
Alvin Wan <hi@alvinwan.com>
2015-07-16 15:16:31 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'datatools',
+  packages = ['datatools'],
+  version = '0.1',
+  description = 'datatools for UC Berkeley\'s DS10 course',
+  author = 'John DeNero, David Culler, Alvin Wan',
+  author_email = '',
+  url = 'https://github.com/alvinwan/datatools',
+  download_url = 'https://github.com/github/alvinwan/tarball/0.1',
+  keywords = ['data', 'tools', 'berkeley'],
+  classifiers = [],
+)
