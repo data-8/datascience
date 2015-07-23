12461e5b4d862f7d75f2e8d2f8d4e7b8452c79e8
Alvin Wan <hi@alvinwan.com>
2015-07-23 02:33:55 -0700

    started tests, map.py more dynamic

--- datascience/maps.py
+++ datascience/maps.py
@@ -1,74 +1,152 @@
-"""Integrates the folium package to draw maps."""
+"""
+
+Integrates the folium package to draw maps.
+
+"""
 
 from IPython.core.display import HTML
 import folium
 
-MAP_WIDTH = 960
-MAP_HEIGHT = 500
+#########
+# Utils #
+#########
 
 
-def draw_map(center, zoom=17, points=[], regions=[]):
-    """Draw a map with center & zoom containing all points and
-    regions that displays points as circles and regions as polygons.
-
-    center -- lat-long pair at the center of the map
-    zoom -- zoom level
-    points -- a list of MapPoints
-    regions -- a list of MapRegions
-
-    TODO(sam): Add regions functionality
+def map_to_defaults(defaults, params, excludes=[]):
     """
-    map = folium.Map(
-        location=center,
-        tiles='Stamen Toner',
-        zoom_start=zoom,
-        width=MAP_WIDTH,
-        height=MAP_HEIGHT
-    )
+    Squishes together two dictionaries, with one containing defaults
+    """
+    rval = defaults.copy()
+    rval.update(params)
+    return {k: v for k, v in rval.items() if k not in excludes}
 
-    for point in points:
-        map.circle_marker(**point.attributes)
+
+def hook(f):
+    """
+    Decorator for obj method, checks for PREFIX_before and PREFIX_after 
+    a method is called 
+    """
+
+    def call_hook(self, template):
+        fn = getattr(self, template % f.__name__, None)
+        if callable(fn):
+            fn()
+
+    def method(self, *args, **kwargs):
+        call_hook(self, 'before_%s')
+        rval = f(self, *args, **kwargs)
+        call_hook(self, 'after_%s')
+        return rval
+    
+    method.__name__ = getattr(f, '__name__', method.__name__)
+    return method
+
+
+class MapError(Exception):
+    
+    message = ''
+    prefix = 'Please report this to your instructors'
+    
+    def get_message(self):
+        return '%s: %s' % (self.prefix, self.message)
+
+
+######################
+# Folium Abstraction #
+######################
+
+
+def draw_map(center, zoom, **kwargs):
+    """ 
+    Draw a map with center & zoom containing all points and
+    regions that displays points as circles and regions as polygons.
+    """
+
+    return Map(
+        location=center, 
+        zoom_start=zoom, 
+        **kwargs).map().to_html()
+
+
+class MapEntity:
+
+    defaults = {}
+    attributes = {} # only data passed to mapper
+    data = {}       # includes all data
+    dataonly = []   # don't pass these variables to the mapper
+    mapper = None   # default function to display this shape on the map
+    folium = None   # access to the map object (kind of)
+
+    def __init__(self, **kwargs):
+        """ Saves attributes, with defaults """
+        self.data = map_to_defaults(self.defaults, kwargs)
+        self.attributes = map_to_defaults(self.defaults, kwargs, excludes=self.dataonly)
+
+    def map(self, map=None):
+        """ Maps this object, with its own attributes """
+        if not callable(self.mapper):
+            self.mapper = getattr(map, self.mapper)
+        self.folium = self.mapper(**self.attributes)
+        return self
+
+
+class Map(MapEntity):
+
+    mapper = folium.Map
+    
+    dataonly = ['points', 'regions']
+
+    defaults = {
+        'location': [45.5244, -122.6699],   # center -- lat-long pair at the center of the map
+        'tiles': 'Stamen Toner',            # zoom -- zoom level
+        'zoom_start': 17,                   # points -- a list of MapPoints
+        'width': 960,
+        'height': 500,
+        'points': [],                       # points -- a list of MapPoints
+        'regions': []                       # regions -- a list of MapRegions
+    }
+
+    @hook
+    def to_html(self):
+        """
+        Takes in a folium map as a parameter and outputs the HTML that
+        IPython will display.
+        """
+        map = self.folium
+        map._build_map()
+        map_html = map.HTML.replace('"', '&quot;')
+        return HTML('<iframe srcdoc="%s" '
+                    'style="width: %spx; height: %spx; '
+                    'border: none"></iframe>' % (map_html, map.width, map.height))
+    
+    def before_to_html(self):
+        """ called before to_html, add points and regions before outputting map """
+        for point in self.data['points']:
+            point.map(self.folium)
+
+        for region in self.data['regions']:
+            region.map(self.folium)
         
-    for region in regions:
-        map.geo_json(**region.attributes)
 
-    return _to_html(map)
-
-
-def _to_html(map):
-    """Takes in a folium map as a parameter and outputs the HTML that
-    IPython will display."""
-    map._build_map()
-    map_html = map.HTML.replace('"', '&quot;')
-    return HTML('<iframe srcdoc="%s" '
-                'style="width: %spx; height: %spx; '
-                'border: none"></iframe>' % (map_html, map.width, map.height))
-
-
-class MapPoint:
-    """A circle. Draw by passing into draw_map."""
-
-    def __init__(self, location, radius=10, popup='', line_color='#3186cc', fill_color='#3186cc', fill_opacity=0.6):
-        """
-        location -- lat-long pair at center of circle
-        radius -- radius of circle on map
-        popup -- text that pops up when circle is clicked
-        line_color -- color of circle border
-        fill_color -- color of circle within border
-        fill_opacity -- opacity of circle fill
-        """
-        self.attributes = {
-            'location': location,
-            'radius': radius,
-            'popup': popup,
-            'line_color': line_color,
-            'fill_color': fill_color,
-            'fill_opacity': fill_opacity
-        }
+class MapPoint(MapEntity):
+    """ A circle. Draw by passing into draw_map."""
+    
+    mapper = 'circle_marker'
+    
+    defaults = {
+        'location': [],             # location -- lat-long pair at center of circle
+        'radius': 10,               # radius -- radius of circle on map
+        'popup': '',                # popup -- text that pops up when circle is clicked
+        'line_color': '#3186cc',    # line_color -- color of circle border
+        'fill_color': '#3186cc',    # fill_color -- color of circle within border
+        'fill_opacity': 0.6         # fill_opacity -- opacity of circle fill
+    }
 
 
-class MapRegion:
-    """A polygon. Draw by passing into draw_map."""
+class MapRegion(MapEntity):
+    """ A polygon. Draw by passing into draw_map."""
+    
+    mapper = 'geo_json'
     
     defaults = {
         'fill_color': 'blue',
@@ -77,9 +155,3 @@ class MapRegion:
         'line_weight': 1,
         'line_opacity': 1
     }
-
-    def __init__(self, **kwargs):
-        """ Sets up a Map Region for folium.geo_json"""
-        self.attributes = self.defaults.copy()
-        for k, v in kwargs.items():
-            self.attributes[k] = v
---
+++ tests/test_map.py
@@ -0,0 +1,24 @@
+from datascience import *
+from unittest.mock import MagicMock, patch
+
+
+def test_draw_map():
+	""" Tests that draw_map returns HTML """
+	region = MapRegion(geo_path='../data/us-states.json')
+	map_html = draw_map((51.5135015, -0.1362392), regions=[region], zoom=3)
+	assert isinstance(map_html, HTML)
+	
+		
+def test_setup_map():
+	""" Tests that to_html() returns HTML """
+	center = [45.5244, -122.6699]
+	kwargs = {
+		'tiles': 'Stamen Toner',            
+		'zoom_start': 17,                   
+		'width': 960,
+		'height': 500,
+		'points': [],                       
+		'regions': []                       
+	}
+	map_html = Map(location=center, **kwargs).map().to_html()
+	assert isinstance(map_html, HTML)
