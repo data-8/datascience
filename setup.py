e87a8885ed4f8fdfa4e5d612155a3ec4b4da5f7a
Alvin Wan <hi@alvinwan.com>
2015-08-04 14:53:08 -0700

    new travis config

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import sys
-import os
-import IPython
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 install_requires = [
-  'numpy',
-  'matplotlib',
-  'pandas',
-  'folium'
+    'numpy',
+    'matplotlib',
+    'pandas',
+    'folium'
 ]
 
-test_requires = [
-  'pytest'
+tests_requires = [
+    'pytest',
+    'coverage',
+    'coveralls'
 ]
+
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
@@ -35,17 +36,17 @@ class PyTest(TestCommand):
 
 
 setup(
-  name = 'datascience',
-  packages = ['datascience'],
-  version = '0.2.3',
-  install_requires = install_requires,
-  tests_require = test_requires,
-  cmdclass = {'test': PyTest},
-  description = 'A Jupyter notebook Python library for introductory data science',
-  author = 'John DeNero, David Culler, Alvin Wan, Sam Lau',
-  author_email = 'ds8-instructors@berkeley.edu',
-  url = 'https://github.com/dsten/datascience',
-  download_url = 'https://github.com/dsten/datascience/archive/0.2.3.zip',
-  keywords = ['data', 'tools', 'berkeley'],
-  classifiers = [],
+    name = 'datascience',
+    packages = ['datascience'],
+    version = '0.2.3',
+    install_requires = install_requires + tests_requires,
+    tests_require = tests_requires,
+    cmdclass = {'test': PyTest},
+    description = 'A Jupyter notebook Python library for introductory data science',
+    author = 'John DeNero, David Culler, Alvin Wan, Sam Lau',
+    author_email = 'ds8-instructors@berkeley.edu',
+    url = 'https://github.com/dsten/datascience',
+    download_url = 'https://github.com/dsten/datascience/archive/0.2.3.zip',
+    keywords = ['data', 'tools', 'berkeley'],
+    classifiers = [],
 )
