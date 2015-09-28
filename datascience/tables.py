a2cd2a015ae5d497ad7ea74837f3d69095666854
Sam Lau <samlau95@gmail.com>
2015-09-28 00:53:20 -0700

    Make repr more accurate
    It was confusing rows with columns before.

--- datascience/tables.py
+++ datascience/tables.py
@@ -639,12 +639,12 @@ class Table(collections.abc.MutableMapping):
             binned[label + ' ' + tag] = np.append(counts, 0)
         return binned
 
-    ##################
-    # Export/Display #
-    ##################
+    ##########################
+    # Exporting / Displaying #
+    ##########################
 
     def __repr__(self):
-        return '<{0}({1} rows): | {3} |>'.format(
+        return '<{0}({1} cols, {2} rows): | {3} |>'.format(
             type(self).__name__,
             len(self),self.num_rows,
             " | ".join(map(str, self.column_labels)))
