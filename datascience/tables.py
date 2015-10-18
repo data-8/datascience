ac38f0183c1c0cf6ad28faa5d7a80c7a86b06a3d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-10-18 02:51:20 -0700

    Correct Table.hist label when counts arg is provided

--- datascience/tables.py
+++ datascience/tables.py
@@ -1580,11 +1580,13 @@ class Table(collections.abc.MutableMapping):
                 bins = np.unique(self[bins])
             vargs['bins'] = bins
 
-        counted_values = None
+        counted_values = counted_label = None
         if counts is not None:
             counted_values = self._get_column(counts)
+            counted_label = 'counts'
             if isinstance(counts, collections.Hashable) and counts in self.column_labels:
                 columns.pop(counts)
+                counted_label = counts
 
         n = len(columns)
         colors = [rgb_color + (self.default_hist_alpha,) for rgb_color in
@@ -1606,11 +1608,12 @@ class Table(collections.abc.MutableMapping):
             for axis, label, color in zip(axes, columns.keys(), colors):
                 if counted_values is None:
                     values = columns[label]
+                    axis.set_xlabel(label, fontsize=16)
                 else:
                     values = counted_values
+                    axis.set_xlabel(counted_label, fontsize=16)
                     vargs['weights'] = columns[label]
                 axis.hist(values, color=color, **vargs)
-                axis.set_xlabel(label, fontsize=16)
 
     def points(self, column__lat, column__long, labels=None, colors=None, **kwargs) :
         latitudes = self._get_column(column__lat)
