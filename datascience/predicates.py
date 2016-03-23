925ba9e8eeca947b008a8ae3fc181b4860feb948
John DeNero <papajohn@gmail.com>
2016-03-22 17:45:02 -0700

    remove both and either

--- datascience/predicates.py
+++ datascience/predicates.py
@@ -1,56 +1,154 @@
 """Predicate functions."""
 
-all = ['both', 'either', 'are']
+import functools
+
+all = ['are']
 
 class are:
-    """Predicate class."""
+    """Predicate functions. The class is named "are" for calls to where.
+
+    For example, given a table, predicates can be used to pick rows as follows.
+
+    >>> from datascience import Table
+    >>> t = Table().with_columns([
+    ...    'Sizes', ['S', 'M', 'L', 'XL'],
+    ...    'Waists', [30, 34, 38, 42],
+    ... ])
+    >>> t.where('Sizes',  are.equal_to('L'))
+    Sizes | Waists
+    L     | 38
+    >>> t.where('Waists', are.above(38))
+    Sizes | Waists
+    XL    | 42
+    >>> t.where('Waists', are.above_or_equal_to(38))
+    Sizes | Waists
+    L     | 38
+    XL    | 42
+    >>> t.where('Waists', are.below(38))
+    Sizes | Waists
+    S     | 30
+    M     | 34
+    >>> t.where('Waists', are.below_or_equal_to(38))
+    Sizes | Waists
+    S     | 30
+    M     | 34
+    L     | 38
+    >>> t.where('Waists', are.strictly_between(30, 38))
+    Sizes | Waists
+    M     | 34
+    >>> t.where('Waists', are.between(30, 38))
+    Sizes | Waists
+    S     | 30
+    M     | 34
+    >>> t.where('Waists', are.between_or_equal_to(30, 38))
+    Sizes | Waists
+    S     | 30
+    M     | 34
+    L     | 38
+    >>> t.where('Sizes',  are.equal_to('L'))
+    Sizes | Waists
+    L     | 38
+    >>> t.where('Waists', are.not_above(38))
+    Sizes | Waists
+    S     | 30
+    M     | 34
+    L     | 38
+    >>> t.where('Waists', are.not_above_or_equal_to(38))
+    Sizes | Waists
+    S     | 30
+    M     | 34
+    >>> t.where('Waists', are.not_below(38))
+    Sizes | Waists
+    L     | 38
+    XL    | 42
+    >>> t.where('Waists', are.not_below_or_equal_to(38))
+    Sizes | Waists
+    XL    | 42
+    >>> t.where('Waists', are.not_strictly_between(30, 38))
+    Sizes | Waists
+    S     | 30
+    L     | 38
+    XL    | 42
+    >>> t.where('Waists', are.not_between(30, 38))
+    Sizes | Waists
+    L     | 38
+    XL    | 42
+    >>> t.where('Waists', are.not_between_or_equal_to(30, 38))
+    Sizes | Waists
+    XL    | 42
+    """
 
     @staticmethod
     def equal_to(y):
         """Equal to y."""
-        return lambda x: x == y
+        return _combinable(lambda x: x == y)
 
     @staticmethod
     def above(y):
         """Greater than y."""
-        return lambda x: x > y
+        return _combinable(lambda x: x > y)
 
     @staticmethod
     def below(y):
         """Less than y."""
-        return lambda x: x < y
+        return _combinable(lambda x: x < y)
 
     @staticmethod
     def above_or_equal_to(y):
         """Greater than or equal to y."""
-        return lambda x: x >= y
+        return _combinable(lambda x: x >= y)
 
     @staticmethod
     def below_or_equal_to(y):
         """Less than or equal to y."""
-        return lambda x: x <= y
+        return _combinable(lambda x: x <= y)
 
     @staticmethod
     def strictly_between(y, z):
         """Greater than y and less than z."""
-        return lambda x: y < x < z
+        return _combinable(lambda x: y < x < z)
 
     @staticmethod
     def between(y, z):
         """Greater than or equal to y and less than z."""
-        return lambda x: y <= x < z
+        return _combinable(lambda x: y <= x < z)
 
     @staticmethod
     def between_or_equal_to(y, z):
         """Greater than or equal to y and less than or equal to z."""
-        return lambda x: y <= x <= z
+        return _combinable(lambda x: y <= x <= z)
+
+###############
+# Combination #
+###############
+
+class _combinable:
+    """A wrapper that allows one-arg predicate functions to be combined."""
+    def __init__(self, f):
+        self.f = f
+        functools.update_wrapper(self, f)
+
+    def __call__(self, x):
+        return self.f(x)
+
+    def __and__(self, other):
+        return _combinable(lambda x: self.f(x) and other.f(x))
+
+    def __or__(self, other):
+        return _combinable(lambda x: self.f(x) or other.f(x))
+
+    def __neg__(self):
+        return _combinable(lambda x: not self.f(x))
+
+    def __xor__(self, other):
+        return (self & -other) | (-self & other)
 
 ############
 # Negation #
 ############
 
 def _not(f):
-    return lambda x: not f(x)
+    return lambda *args: -f(*args)
 
 are.not_equal_to = _not(are.equal_to)
 are.not_above = are.below_or_equal_to
@@ -60,26 +158,3 @@ are.not_above_or_equal_to = are.below
 are.not_strictly_between = _not(are.strictly_between)
 are.not_between = _not(are.between)
 are.not_between_or_equal_to = _not(are.between_or_equal_to)
-
-###############
-# Combination #
-###############
-
-def both(f, g):
-    """Both f and g."""
-    f, g = _wrap_equal_to(f), _wrap_equal_to(g)
-    return lambda x: f(x) and g(x)
-
-def either(f, g):
-    """Either f or g."""
-    f, g = _wrap_equal_to(f), _wrap_equal_to(g)
-    return lambda x: f(x) or g(x)
-
-def _wrap_equal_to(f):
-    if not callable(f):
-        return are.equal_to(f)
-    else:
-        return f
-
-are.both = both
-are.either = either
--- tests/test_predicates.py
+++ tests/test_predicates.py
@@ -4,27 +4,15 @@ from datascience import *
 
 def test_both():
     """Both f and g."""
-    p = both(are.above(2), are.below(4))
+    p = are.above(2) & are.below(4)
     ps = [p(x) for x in range(1, 6)]
     assert ps == [False, False, True, False, False]
 
-def test_both_constant():
-    """Both f and g."""
-    p = both(are.below(3), 2)
-    ps = [p(x) for x in range(1, 6)]
-    assert ps == [False, True, False, False, False]
-
 def test_either():
     """Either f or g."""
-    p = either(are.above(3), are.below(2))
+    p = are.above(3) | are.below(2)
     ps = [p(x) for x in range(1, 6)]
     assert ps == [True, False, False, True, True]
-
-def test_either():
-    """Either f or g."""
-    p = either(are.above(3), 2)
-    ps = [p(x) for x in range(1, 6)]
-    assert ps == [False, True, False, True, True]
 
 def test_equal_to():
     """Equal to y."""
