09f6783f88b08c63bf66c315f39dfee0e6eed7e5
John DeNero <papajohn@gmail.com>
2016-03-29 21:34:31 -0700

    fix percentile

--- datascience/util.py
+++ datascience/util.py
@@ -11,25 +11,31 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from scipy import optimize
 import functools
+import math
 
 
 def percentile(p, arr=None):
     """Returns the pth percentile of the input array (the value that is at
-    least as great as p% of the values in the array)
+    least as great as p% of the values in the array).
 
     If arr is not provided, percentile returns itself curried with p
 
-    >>> percentile(67, [1, 3, 5, 9])
-    9
-    >>> percentile(66, [1, 3, 5, 9])
+    >>> percentile(74.9, [1, 3, 5, 9])
     5
-    >>> f = percentile(66)
+    >>> percentile(75, [1, 3, 5, 9])
+    5
+    >>> percentile(75.1, [1, 3, 5, 9])
+    9
+    >>> f = percentile(75)
     >>> f([1, 3, 5, 9])
     5
     """
     if arr is None:
         return lambda arr: percentile(p, arr)
-    return np.percentile(arr, p, interpolation='higher')
+    if hasattr(p, '__iter__'):
+        return np.array([percentile(x, arr) for x in p])
+    i = (p/100) * len(arr)
+    return arr[math.ceil(i) - 1]
 
 
 def plot_normal_cdf(rbound=None, lbound=None, mean=0, sd=1):
--- tests/test_util.py
+++ tests/test_util.py
@@ -11,17 +11,17 @@ def test_doctests():
 
 
 def test_percentile():
-    assert ds.percentile(0, [1, 3, 5, 9]) == 1
-    assert ds.percentile(33, [1, 3, 5, 9]) == 3
-    assert ds.percentile(34, [1, 3, 5, 9]) == 5
-    assert ds.percentile(66, [1, 3, 5, 9]) == 5
-    assert ds.percentile(67, [1, 3, 5, 9]) == 9
+    assert ds.percentile(25, [1, 3, 5, 9]) == 1
+    assert ds.percentile(26, [1, 3, 5, 9]) == 3
+    assert ds.percentile(55, [1, 3, 5, 9]) == 5
+    assert ds.percentile(75, [1, 3, 5, 9]) == 5
+    assert ds.percentile(76, [1, 3, 5, 9]) == 9
 
-    f1 = ds.percentile(67)
-    assert f1([1, 3, 5, 9]) == 9
+    f1 = ds.percentile(66)
+    assert f1([1, 3, 5, 9]) == 5
 
-    f2 = ds.percentile(66)
-    assert f2([1, 3, 5, 9]) == 5
+    f2 = ds.percentile([65, 85])
+    assert np.all(f2([1, 3, 5, 9]) == np.array([5, 9]))
 
 
 def test_table_apply():
