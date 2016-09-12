0c111d234c700a2c3800aeb141c528d1b4b301ec
John DeNero <papajohn@gmail.com>
2016-09-11 22:06:14 -0700

    remove rogue print from overlain hist

--- datascience/tables.py
+++ datascience/tables.py
@@ -2049,7 +2049,6 @@ class Table(collections.abc.MutableMapping):
                 values = np.repeat(counted_values, n).reshape(-1,n)
             vargs.setdefault('histtype', 'stepfilled')
             figure = plt.figure(figsize=(6, 4))
-            print(values, colors, vargs)
             plt.hist(values, color=colors, **vargs)
             axis = figure.get_axes()[0]
             _vertical_x(axis)
