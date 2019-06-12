1497c271230a311c799400a4db67e018d3c07849
David Wagner <3349841+davidwagner@users.noreply.github.com>
2019-06-11 17:23:45 -0700

    Revert "Update references to github.com/dsten to github.com/data-8, to reflect the rename that we made a while ago."
    This reverts commit da8b132361e554f7c877d4de5fdae3e1b8693ff1.

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
     description = 'A Jupyter notebook Python library for introductory data science',
     author = 'John DeNero, David Culler, Alvin Wan, Sam Lau',
     author_email = 'ds8-instructors@berkeley.edu',
-    url = 'https://github.com/data-8/datascience',
-    download_url = 'https://github.com/data-8/datascience/archive/%s.zip' % version,
+    url = 'https://github.com/dsten/datascience',
+    download_url = 'https://github.com/dsten/datascience/archive/%s.zip' % version,
     keywords = ['data', 'tools', 'berkeley'],
     classifiers = []
 )
