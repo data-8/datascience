70c7cb337fb2ec76f1c56989f5c50a349fe52a69
Adnan Hemani <adnan.h@berkeley.edu>
2020-06-14 20:15:01 -0700

    debugging, pt. 5

--- tests/notebook_runner.py
+++ tests/notebook_runner.py
@@ -5,6 +5,6 @@ from nbconvert.preprocessors import ExecutePreprocessor
 with open(sys.argv[1]) as f:
     nb = nbformat.read(f, as_version=4)
 
-ep = ExecutePreprocessor(timeout=600, kernel_name='python3.6')
+ep = ExecutePreprocessor(timeout=600)
 
 ep.preprocess(nb, {'metadata': {'path': 'tests/'}})
