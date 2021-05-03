795a7bb8494e07d2f56fb96648c67dd3a189ad9f
AASHRUTI SINGH <AASHRUTI.Singh@ibm.com>
2021-05-03 16:54:45 +0530

    Fix Doc for show() method

---
+++ datascience/Displaying methods.py
@@ -0,0 +1,23 @@
+
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [2,8,3,7]) #(x,y) axis division inputs 
+plt.show() #method usage
+
+"""
+   Method: show() - Used for visualizing figure/ figures.
+   Args:
+     No argument 
+
+   Returns:
+     No value returned
+     
+   Disclaimer: plt.show() method should be used only once per session (usually at the end), as Multiple usage of show() can lead to blocking at the backend
+"""
+
+
+
+
+
+
